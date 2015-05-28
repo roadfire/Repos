ef036ab8b30b52c38425515db71a021edc456499
Josh Brown <josh@roadfiresoftware.com>
2015-05-28 15:18:37 -0400

    added DateFormatterTests to compare XCTest to Quick

---
+++ ReposTests/DateFormatterTests.swift
@@ -0,0 +1,25 @@
+//
+//  DateFormatterTests.swift
+//  Repos
+//
+//  Created by Josh Brown on 5/28/15.
+//  Copyright (c) 2015 Roadfire Software. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DateFormatterTests: XCTestCase {
+    func testFormatDateForServer() {
+        let components = NSDateComponents()
+        components.year = 2015
+        components.month = 5
+        components.day = 27
+        
+        let date = NSCalendar.currentCalendar().dateFromComponents(components)!
+        let formatter = DateFormatter()
+        let formattedDate = formatter.stringFromDate(date)
+        
+        XCTAssertEqual(formattedDate, "2015-05-27", "expected date to be '20150527'")
+    }
+}
